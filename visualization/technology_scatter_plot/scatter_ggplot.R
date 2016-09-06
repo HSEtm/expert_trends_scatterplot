1e879775c9b28c222978241503ed1924b1cf9bd2
eisenbart <eisenbart@ya.ru>
2016-09-06 12:27:27 +0300

    minor enhancement - dynamic limits and intercept lines

--- visualization/technology_scatter_plot/scatter_ggplot.R
+++ visualization/technology_scatter_plot/scatter_ggplot.R
@@ -14,18 +14,26 @@ table <- read.table("data.csv",
 table$technology <- paste(toupper(substring(table$technology, 1,1)),
                           substring(table$technology, 2), sep = "")
 
+
+x_lim <- c(floor(min(table$russian_level)),
+           ceiling(max(table$russian_level)))
+
+y_lim <- c(floor(min(table$russian_importance)),
+           ceiling(max(table$russian_importance)))
+
 ggplot(table, # data frame
        aes(x = russian_level,
            y = russian_importance,
            color = priority_area,
            label = technology)) +
-  geom_point() +
-  geom_text(color = 'black',
-         check_overlap = TRUE) +
-  labs(color = "color legend",
-       x = "x legend",
-       y = "y legend") +
-  scale_color_manual(values = unique(table$color, incomparables = FALSE)) +
-  theme(legend.position = "bottom") +
-  lims(x=c(1,4),y=c(1,4.5)) +
-  geom_vline(xintercept = 2.5) + geom_hline(yintercept = 2.75)
+    geom_point() +
+    geom_text(color = 'black',
+              check_overlap = TRUE) +
+    labs(color = "color legend",
+         x = "x legend",
+         y = "y legend") +
+    scale_color_manual(values = unique(table$color, incomparables = FALSE)) +
+    theme(legend.position = "bottom") +
+    lims(x = x_lim, y = y_lim) +
+    geom_vline(xintercept = mean(x_lim)) +
+    geom_hline(yintercept = mean(y_lim))
