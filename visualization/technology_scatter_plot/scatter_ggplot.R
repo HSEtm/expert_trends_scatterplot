4a51264915d339f35c549dd312fe49ce1e31a210
eisenbart <eisenbart@ya.ru>
2016-09-06 12:21:22 +0300

    and finally adding manual colors

--- visualization/technology_scatter_plot/scatter_ggplot.R
+++ visualization/technology_scatter_plot/scatter_ggplot.R
@@ -25,7 +25,7 @@ ggplot(table, # data frame
   labs(color = "color legend",
        x = "x legend",
        y = "y legend") +
-  scale_color_manual(values=color) +
+  scale_color_manual(values = unique(table$color, incomparables = FALSE)) +
   theme(legend.position = "bottom") +
   lims(x=c(1,4),y=c(1,4.5)) +
   geom_vline(xintercept = 2.5) + geom_hline(yintercept = 2.75)
